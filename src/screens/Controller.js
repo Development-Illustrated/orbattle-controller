47e7b16a19564cac97a7608b81e409a6188efca3
Daniel Neenan <daniel@automise.co.uk>
2018-11-10 18:51:06 +0000

    Add buttons to controller screen and the turn colours and shit

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -3,7 +3,9 @@ import React from 'react'
 import {
   View,
   Text,
-  Dimensions
+  Dimensions,
+  ImageBackground,
+  Image
 } from 'react-native'
 
 import { Constants } from 'expo'
@@ -13,7 +15,7 @@ import {
 } from '../components/Container'
 const styles = {
   text: {
-    color: '#FFF',
+    color: 'black',
     textTransform: 'uppercase',
     transform: [{ rotate: '90deg' }]
   },
@@ -31,29 +33,53 @@ const RIGHT = WINDOW_HEIGHT * 0.5
 class Controller extends React.Component {
   constructor () {
     super()
-    this.state = {}
+    this.state = {
+      firing: false,
+      turningLeft: false,
+      turningRight: true
+    }
   }
 
   render () {
+    let baseControls = require('../../assets/unpress-left-right-button.png')
+    let baseFire = require('../../assets/unpress-red-button.png')
+    let turningLeft = require('../../assets/press-left-button.png')
+    let turningRight = require('../../assets/press-right-button.png')
+    let turningSomeWhere = require('../../assets/press-left-right-button.png')
+    let firing = require('../../assets/pressed-red-button.png')
+
+    let fireAllWeapons = this.state.firing ? firing : baseFire
+
+    let movingAllOverTheWorld
+    if (this.state.turningLeft && this.state.turningRight) movingAllOverTheWorld = turningSomeWhere
+    else if (this.state.turningLeft) movingAllOverTheWorld = turningLeft
+    else if (this.state.turningRight) movingAllOverTheWorld = turningRight
+    else movingAllOverTheWorld = baseControls
+
+
     return (
       <Container>
-        <View style={{ flex: 1 }}
-          onMoveShouldSetResponder={() => true}
-          onResponderRelease={() => true}
-          onResponderMove={(evt) => this.moveAndMurder(evt)}
+        <ImageBackground
+          source={require('../../assets/scratched-metal.jpg')}
+          style={{width: '100%', height: '100%', flex: 1}}
+          imageStyle={{resizeMode: 'stretch'}}
+          resizeMode="stretch"
         >
-          <View style={{ flex: 1, flexDirection: 'column' }}>
-            <View style={[styles.textContainer, { backgroundColor: 'blue' }]}>
-              <Text style={styles.text}>Left</Text>
-            </View>
-            <View style={[styles.textContainer, { backgroundColor: 'green' }]}>
-              <Text style={styles.text}>Right</Text>
+          <View style={{ flex: 1 }}
+            onMoveShouldSetResponder={() => true}
+            onResponderRelease={() => this.releaseController()}
+            onResponderMove={(evt) => this.moveAndMurder(evt)}
+          >
+              <View style={{ flex: 1, flexDirection: 'column' }}>
+                <View style={{ width: '100%', height: '50%' }}>
+                  <Image source={ movingAllOverTheWorld } style={{ width: '100%', height: '100%', transform: [{ rotate: '90deg'}] }}/>
+                </View>
+                <View style={{ width: '100%', height: '50%' }}>
+                  <Image source={ fireAllWeapons } style={{ width: '100%', height: '100%', transform: [{ rotate: '90deg'}] }} />
+              </View>
             </View>
           </View>
-          <View style={[styles.textContainer, { backgroundColor: 'red' }]}>
-            <Text style={styles.text}>Fire</Text>
-          </View>
-        </View>
+        </ImageBackground>
       </Container>
     )
   }
@@ -61,11 +87,29 @@ class Controller extends React.Component {
   moveAndMurder ({ nativeEvent }) {
     if (nativeEvent.pageY < LEFT) {
       this.sendRequest('LEFT')
+      this.setState({
+        turningLeft: true
+      })
     } else if (nativeEvent.pageY > LEFT && nativeEvent.pageY < RIGHT) {
       this.sendRequest('RIGHT')
+      this.setState({
+        turningRight: true
+      })
     } else if (nativeEvent.pageY > RIGHT) {
       this.sendRequest('FIRE')
+      this.setState({
+        firing: true
+      })
     }
+  }
+
+  releaseController () {
+    this.setState({
+      turningLeft: false,
+      turningRight: false,
+      firing: false
+    })
+    return true
   }
 
   sendRequest (type) {
